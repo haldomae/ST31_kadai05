81c6d0bea5fe7b356f790f03cc418ca06cb5f105
haldomae <hal.domae@gmail.com>
2025-06-23 13:33:34 +0900

    ResultActivityにライフサイクルを追加

--- app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai05/ResultActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai05/ResultActivity.kt
@@ -1,6 +1,7 @@
 package com.classnumber_00_domaekazuki.st31_kadai05
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("LifeCycle", "ResultActivityのonCreateが実行")
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_result)
@@ -16,5 +18,29 @@ class ResultActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i("LifeCycle", "ResultActivityのonStartが実行")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i("LifeCycle", "ResultActivityのonResumeが実行")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i("LifeCycle", "ResultActivityのonPauseが実行")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i("LifeCycle", "ResultActivityのonStopが実行")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("LifeCycle", "ResultActivityのonRestartが実行")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("LifeCycle", "ResultActivityのonDestroyが実行")
     }
 }
