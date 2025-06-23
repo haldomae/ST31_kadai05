969cd95672ee9f6f48719ecc70d9098d92ea0977
haldomae <hal.domae@gmail.com>
2025-06-23 11:21:42 +0900

    onCreateにコメント記載

--- app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai05/MainActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai05/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.classnumber_00_domaekazuki.st31_kadai05
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    // Activityが生成される時に実行される
+    // 最初にしか実行しない
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("LifeCycle", "MainActivityのonCreateが実行")
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
