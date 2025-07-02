62f5c38b6a100a24cdd396fa5dac3272685df7d5
haldomae <hal.domae@gmail.com>
2025-07-02 09:58:28 +0900

    onCreateにコメントを記載

--- app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai05/MainActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai05/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.classnumber_00_domaekazuki.st31_kadai05
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    // 画面が生成されたときに実行される
+    // 画面生成時、一度しか実行されない
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("LifeCycle", "MainActivityのonCreateが実行")
         super.onCreate(savedInstanceState)
+        // アプリフルスクリーンで表示
         enableEdgeToEdge()
+        // どのxmlファイル使うか
         setContentView(R.layout.activity_main)
+        // 余白いい感じにしてくれる
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
